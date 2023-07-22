2ed5e23a9fa564db273fa8d876361f9feabf449b
Tzu-An Sheng <tasheng@mit.edu>
2023-07-22 08:14:07 -0400

    Auxiliary scripts

--- gatherw.py
+++ gatherw.py
@@ -1,28 +1,92 @@
 #! /bin/python
 import ROOT as r
 import shutil
-ptlist = [5, 7, 10, 15, 20, 50, 60]
+import re
+# ptlist = [5, 7, 10, 15, 20, 50, 60]
+# ptlist = [5, 7, 10, 15, 20, 60]
 
-outFiles = ['BPw.root', 'Bsw.root']
+# outFiles = ['BPw.root', 'Bsw.root']
 
-fout = r.TFile(outFiles[0], 'recreate')
-for pti, ptf in zip(ptlist[:-1], ptlist[1:]):
-    fin = r.TFile(f'./results/Bu/{pti}_{ptf}/mc_validation_plots/weights/weights.root')
-    h = fin.Get(f'weights_BDT_pt_{pti}_{ptf}')
-    fout.cd()
-    h.Write()
-fout.Close()
+# fout = r.TFile(outFiles[0], 'recreate')
+# for pti, ptf in zip(ptlist[:-1], ptlist[1:]):
+#     fin = r.TFile(f'./results/Bu/{pti}_{ptf}/mc_validation_plots/weights/weights.root')
+#     h = fin.Get(f'weights_BDT_pt_{pti}_{ptf}')
+#     h2 = h.Clone()
+#     fout.cd()
+#     h2.Write()
+# fout.Close()
 
-ptlist = [7, 10, 15, 20, 50]
-fout = r.TFile(outFiles[1], 'recreate')
-for pti, ptf in zip(ptlist[:-1], ptlist[1:]):
-    fin = r.TFile(f'./results/Bs/{pti}_{ptf}/mc_validation_plots/weights/weights.root')
-    h = fin.Get(f'weights_BDT_pt_{pti}_{ptf}')
-    fout.cd()
-    h.Write()
-fout.Close()
+# ptlist = [7, 10, 15, 20, 50]
+# fout = r.TFile(outFiles[1], 'recreate')
+# for pti, ptf in zip(ptlist[:-1], ptlist[1:]):
+#     fin = r.TFile(f'./results/Bs/{pti}_{ptf}/mc_validation_plots/weights/weights.root')
+#     h = fin.Get(f'weights_BDT_pt_{pti}_{ptf}')
+#     h2 = h.Clone()
+#     fout.cd()
+#     h2.Write()
+# fout.Close()
 
-shutil.copy2(outFiles[0], '../gd/braa_nohbhe_trk/BP/EffAna/BDTWeights/BPw.root')
-shutil.copy2(outFiles[0], '../gd/braa_mergebin/BP/EffAna/BDTWeights/BPw.root')
-shutil.copy2(outFiles[1], '../gd/braa_nohbhe_trk/Bs/EffAna/BDTWeights/Bsw.root')
-shutil.copy2(outFiles[1], '../gd/braa_mergebin/Bs/EffAna/BDTWeights/Bsw.root')
+# shutil.copy2(outFiles[0], '../gd/braa_nohbhe_trk/BP/EffAna/BDTWeights/BPw.root')
+# shutil.copy2(outFiles[0], '../gd/braa_mergebin_git/BP/EffAna/BDTWeights/BPw.root')
+# shutil.copy2(outFiles[1], '../gd/braa_nohbhe_trk/Bs/EffAna/BDTWeights/Bsw.root')
+# shutil.copy2(outFiles[1], '../gd/braa_mergebin_git/Bs/EffAna/BDTWeights/Bsw.root')
+
+
+bpptlist = [5, 15, 60]
+bsptlist = [7, 15, 50]
+combptlist = (bpptlist, bsptlist)
+varlist = ['BsvpvSig', 'Bchi2cl', 'Bmumumass']
+btypes = ['BP', 'Bs']
+btypes_low = ['Bp', 'Bs']
+cats = ['Bu', 'Bs']
+
+# for bt, bcat, ptlist in zip(btypes, cats, combptlist):
+#     for pti, ptf in zip(ptlist[:-1], ptlist[1:]):
+#         for v in varlist:
+#             # weight_file = f"./results/{bcat}/inclusive_{pti}_{ptf}_3gauss/mc_validation_plots/weights/weights.root"
+#             # shutil.copy2(weight_file, f'../gd/braa_nohbhe_trk/{bt}/EffAna/BDTWeights/{bt}weights.root')
+#             fcopy = f'./results/{bcat}/inclusive_{pti}_{ptf}_3gauss/mc_validation_plots/mc_sp/pdfs/{v}_mc_validation_{bcat}.pdf'
+#             shutil.copy2(fcopy, f'../overleaf/Plots/Syst/MCData/{bt}/{v}_pt_inclusive_{pti}_{ptf}.pdf')
+#                 # ystr = "_y15-24" if pti < 10 else ""
+#                 # fcopy = f'./results/{bcat}/{pti}_{ptf}/mc_validation_plots/mc_sp/pdfs/{v}_mc_validation_{bcat}{ystr}.pdf'
+#                 # print(fcopy)
+
+#                 # shutil.copy2(fcopy, f'../overleaf/Plots/Syst/MCData/BP/{v}_pt_{pti}_{ptf}.pdf')
+
+#         # fcopy = f'/home/tasheng/braa/BinQGP_trk5/results/Bu/{pti}_{ptf}/mc_validation_plots/mc_sp/pdfs/BDT_pt_{pti}_{ptf}_mc_validation_Bu{ystr}.pdf'
+#         # print(fcopy)
+#         # shutil.copy2(fcopy, f'../overleaf/Plots/Syst/MCData/BP/')
+
+
+varlist = ['BsvpvDistRatio', 'Bchi2cl']
+outFiles = ['BPweights.root', 'Bsweights.root']
+suffix = ['Low', 'High']
+for bt, bcat, ptlist, outstr in zip(btypes, cats, combptlist, outFiles):
+    for pti, ptf, suf in zip(ptlist[:-1], ptlist[1:], suffix):
+        outfile = outstr[:-5] + suf + '.root'
+        fout = r.TFile(outfile, 'recreate')
+        rep = '_rep' if bt == 'BP' else ''
+        for v in varlist:
+            infile = f'./results/{bcat}/inclusive_{pti}_{ptf}_{v}{rep}/mc_validation_plots/weights/weights.root'
+            # infile = f'./results/{bcat}/inclusive_{pti}_{ptf}/mc_validation_plots/weights/weights.root'
+
+            fin = r.TFile(infile)
+            # print(infile)
+            if v == 'BsvpvDistRatio':
+                oth = 'Bchi2cl'
+            elif v == 'Bchi2cl':
+                oth = 'BsvpvSig'
+            else:
+                print('error')
+            for cate in ['mc', 'data']:
+                h = fin.Get(f'{cate}_{oth}')
+                h2 = h.Clone(f'{cate}_{oth}')
+                # print(h.GetBinContent(1))
+                fout.cd()
+                h2.Write()
+
+            fcopy = f'./results/{bcat}/inclusive_{pti}_{ptf}_{v}{rep}/mc_validation_plots/mc_sp/pdfs/{oth}_mc_validation_{bcat}.pdf'
+            shutil.copy2(fcopy, f'../overleaf/Plots/Syst/MCData/{bt}/{oth}_pt_inclusive_{pti}_{ptf}.pdf')
+        fout.Close()
+        shutil.copy2(outfile, f'../gd/braa_nohbhe_trk/{bt}/EffAna/BDTWeights/')
+        shutil.copy2(outfile, f'../gd/braa_mergebin_git/{bt}/EffAna/BDTWeights/')
--- splitpt.py
+++ splitpt.py
@@ -1,5 +1,8 @@
 # read skimmed files, split them into different pt bins
 import ROOT as r
+import re
+# if 'can' not in globals():
+#     can = r.TCanvas('can', 'can', 600, 600)
 
 # from subprocess import run, PIPE
 
@@ -7,22 +10,44 @@ import ROOT as r
 # finlist = ["BsMC.root", "BsData.root"]
 pre = "/home/tasheng/braa/files/"
 
-bpfinlist = [pre + post + "_noBDT.root" for post in ["BPData", "BPMC"] ]
-bpptlist = [5, 7, 10, 15, 20, 50, 60]
+cat = "_newsample_presel_chi2all"
+bpfinlist = [pre + post + cat + ".root" for post in ["BPData", "BPMC", "jpsinp"] ]
+# bpptlist = [5, 7, 10, 15, 20, 50, 60]
+# bpptlist = [5, 7, 10, 15, 20, 60]
+# bpptlist = [5, 15, 60]
+bpptlist = [5, 60]
 
-bsfinlist = [pre + post + "_noBDT.root" for post in ["BsData", "BsMC"] ]
-bsptlist = [7, 10, 15, 20, 50]
+bsfinlist = [pre + post + cat + ".root" for post in ["BsData", "BsMC"] ]
+# bsptlist = [7, 10, 15, 20, 50]
+# bsptlist = [7, 15, 50]
+bsptlist = [7, 50]
+# bsptlist = [10, 50]
 
-# for fstr in bsfinlist:
+chi2cut = 'Bchi2cl > 0.05'
+dlscut = 'BsvpvDistRatio > 2'
+cutlist = [chi2cut, dlscut]
+
+for filename, treename, ptlist in zip([bpfinlist, bsfinlist],
+                                      ['ntKp', 'ntphi'],
+                                      [bpptlist, bsptlist]):
+    for fstr in filename:
+        for cut in cutlist:
+            cutname = re.findall(r'\w+', cut)[0]
+            dfnt = r.RDataFrame(treename, fstr)
+            for pti, ptf in zip(ptlist[:-1], ptlist[1:]):
+                dfnt.Filter(f'Bpt > {pti} && Bpt < {ptf}')\
+                    .Filter(cut)\
+                    .Snapshot(treename, fstr[:-5] + f'_{pti}_{ptf}_{cutname}.root')
+
+# for fstr in bpfinlist:
 #     fin = r.TFile(fstr)
-#     dfnt = r.RDataFrame('ntphi', fin)
-#     for pti, ptf in zip(bsptlist[:-1], bsptlist[1:]):
+#     dfnt = r.RDataFrame('ntKp', fin)
+#     for pti, ptf in zip(bpptlist[:-1], bpptlist[1:]):
 #         dfnt.Filter(f'Bpt > {pti} && Bpt < {ptf}').Snapshot(
-#             'ntphi', fstr[:-5] + f'_trk5_{pti}_{ptf}.root')
+#             'ntKp', fstr[:-5] + f'_{pti}_{ptf}.root')
 
-for fstr in bpfinlist:
-    fin = r.TFile(fstr)
-    dfnt = r.RDataFrame('ntKp', fin)
-    for pti, ptf in zip(bpptlist[:-1], bpptlist[1:]):
-        dfnt.Filter(f'Bpt > {pti} && Bpt < {ptf}').Snapshot(
-            'ntKp', fstr[:-5] + f'_trk5_{pti}_{ptf}.root')
+# r.gStyle.SetOptStat(0)
+# th2 = dfnt.Histo2D(( 'mod', ';Bchi2cl;DLS', 100, 0, 1, 100, 0, 80 ), 'Bchi2cl', 'BsvpvDistRatio')
+# th2.SetTitle(f'Cov coef:{th2.GetCovariance():.2f}')
+# th2.Draw('colorz')
+# can.SaveAs('test.png')
